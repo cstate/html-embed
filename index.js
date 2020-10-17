e286a29790a4a0926bbfb2bee49e066f0f963bbd
Mantas Vilčinskas <hi@mnts.lt>
2020-10-17 20:02:14 +0300

    Finish v1

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-// HTML Embed for cState v0.1
+// HTML Embed for cState
+// Version 1.0
 // github.com/cstate/html-embed
 
 // Variables
 // Site + '/index.json'
 var cStateAPIRoot = 'https://flamboyant-shirley-6bc75e.netlify.app/index.json'
-var cStateEmbedName = '[cState HTML Embed v0.1] ';
+var cStateEmbedPrefix = '[cState HTML Embed v1.0] ';
+var cStateEmbedDebugging = false;
+var cStateAPIStatus = 'tryingToGetStatus';
 
-var cStateAPIStatus = null;
-
-// Logic
+// Code itself
 fetch(cStateAPIRoot)
-  .then(response => response.json())
-  .then(data =>
-    if (data.summaryStatus !== 'ok') {
-      console.log(cStateEmbedName + 'Overall status: ' + data.summaryStatus)
-      cStateAPIStatus = data.summaryStatus
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log(cStateEmbedPrefix + 'API not OK, it sent HTTP status code ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        
+        // When debugging, this code should be run to see API response
+        if (cStateEmbedDebugging) {
+          console.log(cStateEmbedPrefix + 'API response: ', data);
+          cStateAPIStatus = data.summaryStatus;
+          console.log(cStateEmbedPrefix + 'API says status page is: ' + cStateAPIStatus);
+        }
+          
+      });
     }
-)
+  )
+  .catch(function(err) {
+    console.log('Fetch error :-S', err);
+  });
+
